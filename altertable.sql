1900f4de1dcbf634c0abaa214736cf04fa194a52
AVINASH <86355693+avinash992000@users.noreply.github.com>
2021-07-28 13:38:08 +0530

    Update altertable.sql

--- altertable.sql
+++ altertable.sql
@@ -1,1 +1,23 @@
+create table employee(eno int primary key,ename varchar(20),salary int);  -->creating table of employee
+describe employee; -->it will describes type of the tuples in the table
+insert into employee values(1,avinash,900000);  -->insertion of values into the table
+insert into employee values(2,raj,700000);
+insert into employee values(100,mounika,850000);
+insert into employee values(100,ram,850000);    -->here ram will not be inserted into employee because eno should not be same with previous values so please check data type while inserting values into the table
+insert into employee values(101,padma,850000);
+insert into employee values(3,srm,850000);
+select * from employee; -->it displays data in table
 
+--> Here we look into how we can update and delete data from database
+
+update employee set salary=1000000 where eno=1;  -->it will update the salary of avinash
+delete from employee where eno=3;-->it will delete that particular row from employee table
+
+-->Here we try to add and delete and modify and rename tuples of table employee basicaly we try to see different types of using alter attribute
+
+alter table employee add etype char(5);-->it will add etype coloum to the table employee
+alter table employee add constrain UK unique(etype);-->it will add unique constrain to it means it will not allow duplicate values it can be a null
+alter table employee modify etype char(20)-->basically here the size of the etype will be modified
+alter table employee rename column etype to emptype;-->it we renames the column etype to emptype
+alter table employee drop constrain UK;-->the constrain will be dropped
+alter table employee drop column emptype;-->column emptype will be deleted
