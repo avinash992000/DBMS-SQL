2f6cf6ffeb1c91984e70a6f6bcb5bec7bbabbfe1
AVINASH <86355693+avinash992000@users.noreply.github.com>
2021-07-28 13:41:53 +0530

    Update createandinsert.sql

--- createandinsert.sql
+++ createandinsert.sql
@@ -1,6 +1,6 @@
-create table employee(eno int primary key,ename varchar(20),salary int);->creating table of employee
-describe employee;->it will describes type of the tuples in the table
-insert into employee values(1,avinash,900000);->insertion of values into the table
+create table employee(eno int primary key,ename varchar(20),salary int);   -->creating table of employee
+describe employee;   -->it will describes type of the tuples in the table
+insert into employee values(1,avinash,900000);    -->insertion of values into the table
 insert into employee values(2,raj,700000);
 insert into employee values(100,mounika,850000);
-select * from employee;->it displays data in table
+select * from employee;    -->it displays data in table
