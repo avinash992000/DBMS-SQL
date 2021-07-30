b527ad36d5e2b9d1a49cf66eddb417cc04d68ee5
AVINASH <86355693+avinash992000@users.noreply.github.com>
2021-07-30 13:07:16 +0530

    Update aggregatefunction.sql

--- aggregatefunction.sql
+++ aggregatefunction.sql
@@ -8,4 +8,13 @@ insert into employee values(101,padma,550000);
 insert into employee values(3,srm,850000);
 select * from employee; -->it displays data in table
 
--->
+-->aggregate function are max()-int type,min()->int type,avg()->int/char type,count()->no of rows,sum()->int type
+select min(salary) from employee;-->here column name will appear as MIN(salary) and minimum value in salary column will be returned
+select min(salary) as minimum from employee;-->here column name will appear as minimum
+select max(salary) from employee;-->here column name will appear as MAX(salary) and maximum value in salary column will be returned
+select avg(salary) from employee;-->here column name will appear as AVG(salary) and average value in salary column will be returned
+select sum(salary) from employee;-->here column name will appear as SUM(salary) and maximum value in salary column will be returned
+select count(salary) from employee;-->here column name will appear as COUNT(salary) and return num of row in salary
+select count(10) from employee;-->here column name will appear as COUNT(10) and return num of row in employee table
+
+-->NOTE:nested statements like min(sum(eno,salary) can be used only with group by statements
