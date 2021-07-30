40411fb234ea5483b92992865df8b4f9d16544e8
AVINASH <86355693+avinash992000@users.noreply.github.com>
2021-07-30 12:11:15 +0530

    Create aggregatefunction.sql

---
+++ aggregatefunction.sql
@@ -0,0 +1,11 @@
+create table employee(eno int primary key,ename varchar(20),salary int);  -->creating table of employee
+describe employee; -->it will describes type of the tuples in the table
+insert into employee values(1,avinash,900000);  -->insertion of values into the table
+insert into employee values(2,raj,700000);
+insert into employee values(100,mounika,850000);
+insert into employee values(100,ram,850000);    -->here ram will not be inserted into employee because eno should not be same with previous values so please check data type while inserting values into the table
+insert into employee values(101,padma,550000);
+insert into employee values(3,srm,850000);
+select * from employee; -->it displays data in table
+
+-->
